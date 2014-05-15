9307529b51a2dba297ffbea773ef9cfec3dcd1a7
Christian Lück <christian@lueck.tv>
2014-05-15 10:38:18 +0200

    Simplify data structures by removing AbstractResponseDecorator

--- src/Message/Response/BufferedResponse.php
+++ src/Message/Response/BufferedResponse.php
@@ -4,19 +4,44 @@ namespace Clue\React\Buzz\Message\Response;
 
 use React\HttpClient\Response as ResponseStream;
 
-class BufferedResponse extends AbstractResponseDecorator
+class BufferedResponse implements ResponseInterface
 {
+    private $response;
     private $body = '';
 
     public function __construct(ResponseStream $response)
     {
-        parent::__construct($response);
-
+        $this->response = $response;
         $body     =& $this->body;
         $response->on('data', function ($data) use (&$body) {
             $body .= $data;
             // progress
         });
+    }
+
+    public function getProtocol()
+    {
+        return $this->response->getProtocol();
+    }
+
+    public function getVersion()
+    {
+        return $this->response->getVersion();
+    }
+
+    public function getCode()
+    {
+        return $this->response->getCode();
+    }
+
+    public function getReasonPhrase()
+    {
+        return $this->response->getReasonPhrase();
+    }
+
+    public function getHeaders()
+    {
+        return $this->response->getHeaders();
     }
 
     public function getBody()
