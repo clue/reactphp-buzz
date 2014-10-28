4014b7df21e7f0c6f25d8e49fc308da203955a16
Christian Lück <christian@lueck.tv>
2014-10-29 00:39:29 +0100

    Update SOCKS example (fixes #17)

--- examples/socks/socks.php
+++ examples/socks/socks.php
@@ -3,8 +3,7 @@
 use Clue\React\Buzz\Io\Sender;
 use React\HttpClient\Client as HttpClient;
 use React\EventLoop\Factory as LoopFactory;
-use React\Dns\Resolver\Factory as DnsFactory;
-use Clue\React\Socks\Factory as SocksFactory;
+use Clue\React\Socks\Client as SocksClient;
 use Clue\React\Buzz\Browser;
 use Clue\React\Buzz\Message\Response;
 
@@ -12,14 +11,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 $loop = LoopFactory::create();
 
-// use Google's public DNS server
-$dnsResolverFactory = new DnsFactory();
-$dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
-
 // create a new SOCKS client which connects to a SOCKS server listening on localhost:9050
 // not already running a SOCKS server? Try this: ssh -D 9050 localhost
-$factory = new SocksFactory($loop, $dns);
-$socks = $factory->createClient('127.0.0.1', 9050);
+$socks = new SocksClient($loop, '127.0.0.1', 9050);
 
 // create a Browser object that uses the SOCKS client for connections
 $http = new HttpClient($loop, $socks->createConnector(), $socks->createSecureConnector());
