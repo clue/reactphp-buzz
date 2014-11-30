d7b7125ff88e9bc247f2ff12237244ef2395b89f
Christian Lück <christian@lueck.tv>
2014-11-30 01:28:08 +0100

    Move loop to test setup

--- tests/Io/SenderTest.php
+++ tests/Io/SenderTest.php
@@ -7,33 +7,35 @@ use Clue\React\Buzz\Message\Request;
 
 class SenderTest extends TestCase
 {
+    private $loop;
+
+    public function setUp()
+    {
+        $this->loop = $this->getMock('React\EventLoop\LoopInterface');
+    }
+
     public function testCreateFromLoop()
     {
-        $loop = $this->getMock('React\EventLoop\LoopInterface');
-
-        $sender = Sender::createFromLoop($loop);
+        $sender = Sender::createFromLoop($this->loop);
 
         $this->assertInstanceOf('Clue\React\Buzz\Io\Sender', $sender);
     }
 
     public function testCreateFromLoopConnectors()
     {
-        $loop = $this->getMock('React\EventLoop\LoopInterface');
         $connector = $this->getMock('React\SocketClient\ConnectorInterface');
 
-        $sender = Sender::createFromLoopConnectors($loop, $connector);
+        $sender = Sender::createFromLoopConnectors($this->loop, $connector);
 
         $this->assertInstanceOf('Clue\React\Buzz\Io\Sender', $sender);
     }
 
     public function testSenderRejection()
     {
-        $loop = $this->getMock('React\EventLoop\LoopInterface');
-
         $connector = $this->getMock('React\SocketClient\ConnectorInterface');
         $connector->expects($this->once())->method('create')->will($this->returnValue($this->createRejected(new RuntimeException('Rejected'))));
 
-        $sender = Sender::createFromLoopConnectors($loop, $connector);
+        $sender = Sender::createFromLoopConnectors($this->loop, $connector);
 
         $request = new Request('GET', 'http://www.google.com/');
 
